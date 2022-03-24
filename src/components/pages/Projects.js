cedd26df13b646d0f2c046a9a4d906b2c2d270ca
rodrigomb13 <rodrigomb013@gmail.com>
2022-03-24 15:34:55 -0300

    codigos

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,58 @@
+import {useLocation} from 'react-router-dom'
+import {useState, useEffect} from 'react'
+
+import styles from './Projects.module.css'
+import Message from '../layout/Message'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
+
+
+function Projects(){
+
+    const [projects, setProjects] = useState([])
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects',{
+            method:'GET',
+            headers:{
+            'Content-type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                setProjects(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
+    return(
+        <div className={styles.project_container}>
+            <div className={styles.title_container}> 
+                <h1> Meus Projetos</h1>
+                <LinkButton to="newproject" text="Criar Projeto"/>
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                {projects.length > 0 && 
+                    projects.map((project) => (
+                        <ProjectCard 
+                        name={project.name} 
+                        id={project.id}
+                        budget={project.budget}
+                        category={project.category.name}
+                        key={project.id} />
+                    ))}
+            </Container>
+        </div>
+    )
+}
+
+export default Projects
